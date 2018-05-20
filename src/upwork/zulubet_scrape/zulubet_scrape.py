b8754259a99a3c5e1e7d3af4f8fc2e5397bd76ad
jrj92280 <jrj92280@gmail.com>
2018-05-20 10:21:38 -0400

    Get dates from tables

--- src/upwork/zulubet_scrape/zulubet_scrape.py
+++ src/upwork/zulubet_scrape/zulubet_scrape.py
@@ -11,12 +11,19 @@ soup = BeautifulSoup(page)
 
 content_table = soup.find('table', attrs={'class': 'content_table'})
 
+game_data = []
+
 for index, row in enumerate(content_table.contents):
     if index <= 1:
         continue
 
-    for row_index, td in enumerate(row.contents):
-        print(str(row_index) + "_" + str(td))
+    for row_column, td in enumerate(row.contents):
+        if row_column == 0:
+            # 'mf_usertime(\\'01/01/2016, 05:15\\');01-01, 06:15'
+            td_data = td.text.split("'")
+            game_data.append(td_data[1])
 
-    print(str(index) + "_" + str(row))
-print(content_table)
+        # print(str(row_column) + "_" + str(td))
+
+    # print(str(index) + "_" + str(row))
+print(game_data)
