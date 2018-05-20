cc4b0e087ee3babf4141a3d766fcff41d0b584df
jrj92280 <jrj92280@gmail.com>
2018-05-19 23:56:48 -0400

    Add env variables

--- src/upwork/linkedin_connections/README.md
+++ src/upwork/linkedin_connections/README.md
@@ -7,3 +7,24 @@ I want to increase my LinkedIn connections.
 ## Solution
 
 Create Selenium to add a user's suggested connections.
+
+### Assumptions
+
+- Random waits will make this program appear less bot like
+- Account has phone number
+
+
+### Steps 
+
+1. go to: www.linkedin.com
+2. enter: user name, password, and sign in
+3. click: My Network
+4. click: Connect for "People you my know"
+
+
+#### Environment Variables
+
+```bash
+USERNAME
+PASSWORD
+```
--- src/upwork/linkedin_connections/create_connections.py
+++ src/upwork/linkedin_connections/create_connections.py
@@ -1,10 +1,28 @@
-import time
-from selenium import webdriver
+import os
 
-for count in range(1,3):
-    driver = webdriver.Chrome()
-    driver.get('http://www.google.com/xhtml');
-    search_box = driver.find_element_by_name('q')
-    search_box.send_keys('ChromeDriver')
-    search_box.submit()
-    driver.quit()
+username = os.environ.get("USERNAME")
+password = os.environ.get('PASSWORD')
+
+if not username or not password:
+    raise RuntimeError('USERNAME & PASSWORD are required environment variables.')
+
+# from selenium import webdriver
+
+# driver = webdriver.Chrome()
+#
+#
+# driver.get('http://www.google.com/xhtml')
+#
+#
+# search_box = driver.find_element_by_name('q')
+# search_box.send_keys('ChromeDriver')
+# search_box.submit()
+# driver.quit()
+
+
+"""
+1. go to: www.linkedin.com
+2. enter: user name, password, and sign in
+3. click: My Network
+4. click: Connect for "People you my know"
+"""
