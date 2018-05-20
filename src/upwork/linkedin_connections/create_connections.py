af3687c7c42ec1961bcba18b279df557243b900a
jrj92280 <jrj92280@gmail.com>
2018-05-20 00:33:41 -0400

    Complete LinkedIn create connections

--- src/upwork/linkedin_connections/create_connections.py
+++ src/upwork/linkedin_connections/create_connections.py
@@ -1,4 +1,8 @@
 import os
+import random
+import time
+
+from selenium import webdriver
 
 username = os.environ.get("USERNAME")
 password = os.environ.get('PASSWORD')
@@ -6,23 +10,43 @@ password = os.environ.get('PASSWORD')
 if not username or not password:
     raise RuntimeError('USERNAME & PASSWORD are required environment variables.')
 
-# from selenium import webdriver
 
-# driver = webdriver.Chrome()
 #
-#
-# driver.get('http://www.google.com/xhtml')
-#
-#
-# search_box = driver.find_element_by_name('q')
-# search_box.send_keys('ChromeDriver')
-# search_box.submit()
-# driver.quit()
+def run():
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)  # seconds
+
+    driver.get('https://www.linkedin.com')
+
+    username_input = driver.find_element_by_id('login-email')
+    username_input.send_keys(username)
+
+    password_input = driver.find_element_by_id('login-password')
+    password_input.send_keys(password)
+
+    sign_in_button = driver.find_element_by_id("login-submit")
+    sign_in_button.click()
+
+    random_sleep()
+
+    my_network_icon = driver.find_element_by_id('mynetwork-tab-icon')
+    my_network_icon.click()
+
+    random_sleep()
+
+    invite_buttons = driver.find_elements_by_css_selector("*[data-control-name='invite']")
+
+    for invite_button in invite_buttons:
+        invite_button.click()
+        random_sleep()
+
+    driver.quit()
 
 
-"""
-1. go to: www.linkedin.com
-2. enter: user name, password, and sign in
-3. click: My Network
-4. click: Connect for "People you my know"
-"""
+def random_sleep():
+    sleep_time = random.randint(2, 7)
+    time.sleep(sleep_time)
+
+
+if __name__ == '__main__':
+    run()
